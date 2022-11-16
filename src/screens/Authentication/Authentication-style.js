0c9686893ce008a9b2e26d6fa20436b352e7afbe
Настя <sulina01@mail.ru>
2022-11-16 13:09:43 +0300

    comment all styles

--- src/screens/Authentication/Authentication-style.js
+++ src/screens/Authentication/Authentication-style.js
@@ -2,46 +2,46 @@ import { StyleSheet } from 'react-native';
 import { COLORS } from '../../styles/globalStyles';
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: COLORS.white,
-        maxWidth: 350,
-        padding: 20,
-        borderRadius: 16,
-    },
-    logo: {
-        width: 110,
-        height: 34,
-        alignSelf: 'center',
-        marginBottom: 20,
-    },
-    eyeButton: {
-        position: 'absolute',
-        right: 16,
-        top: 14,
-    },
-    eyeImage: {
-        width: 24,
-        height: 24,
-    },
-    passwordInput: {
-        width: '100%',
-        marginVertical: 10,
-    },
-    text: {
-        color: COLORS.black,
-        fontSize: 20,
-        marginBottom: 32,
-        textAlign: 'center',
-        fontWeight: 600,
-    },
-    forgotPasswordButton: {
-        paddingTop: 2,
-        paddingBottom: 16,
-    },
-    registrationButton: {
-        paddingBottom: 8,
-        alignItems: 'center',
-    },
+    // container: {
+    //     backgroundColor: COLORS.white,
+    //     maxWidth: 350,
+    //     padding: 20,
+    //     borderRadius: 16,
+    // },
+    // logo: {
+    //     width: 110,
+    //     height: 34,
+    //     alignSelf: 'center',
+    //     marginBottom: 20,
+    // },
+    // eyeButton: {
+    //     position: 'absolute',
+    //     right: 16,
+    //     top: 14,
+    // },
+    // eyeImage: {
+    //     width: 24,
+    //     height: 24,
+    // },
+    // passwordInput: {
+    //     width: '100%',
+    //     marginVertical: 10,
+    // },
+    // text: {
+    //     color: COLORS.black,
+    //     fontSize: 20,
+    //     marginBottom: 32,
+    //     textAlign: 'center',
+    //     fontWeight: 600,
+    // },
+    // forgotPasswordButton: {
+    //     paddingTop: 2,
+    //     paddingBottom: 16,
+    // },
+    // registrationButton: {
+    //     paddingBottom: 8,
+    //     alignItems: 'center',
+    // },
 });
 
 export default styles;
