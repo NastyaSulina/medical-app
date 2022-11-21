fd0b874f19f98672c639391a3c224381a01cc542
Настя <sulina01@mail.ru>
2022-11-22 02:57:34 +0300

    add Menu component

---
+++ src/components/Menu/Menu-styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from '../../styles/globalStyles';
+
+const styles = StyleSheet.create({
+    container: {
+        borderTopRightRadius: 16,
+        borderTopLeftRadius: 16,
+        minHeight: 86,
+        backgroundColor: COLORS.white,
+        minWidth: '100%',
+        marginTop: 12,
+    },
+});
+
+export default styles;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './Menu-styles';
+
+function Menu() {
+    return <View style={styles.container} />;
+}
+
+export default Menu;
