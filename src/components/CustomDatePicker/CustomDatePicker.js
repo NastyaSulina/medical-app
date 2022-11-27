2f8beffbd52d4fe8d17afb2c219766c965e2873f
imgrau <sulina01@mail.ru>
2022-11-28 00:54:26 +0300

    calendar theme

--- src/components/CustomDatePicker/CustomDatePicker.js
+++ src/components/CustomDatePicker/CustomDatePicker.js
@@ -1,26 +1,65 @@
 import React, { useState } from 'react';
-import { Calendar } from 'react-native-calendars';
-import styles from './CustomDatePicker-styles';
-import { COLORS } from '../../styles/globalStyles';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+import theme from './CustomDatePicker-styles';
 
 function CustomDatePicker() {
+    LocaleConfig.locales.ru = {
+        monthNames: [
+            'Январь',
+            'Февраль',
+            'Март',
+            'Апрель',
+            'Май',
+            'Июнь',
+            'Июль',
+            'Август',
+            'Сентябрь',
+            'Октябрь',
+            'Ноябрь',
+            'Декабрь',
+        ],
+        monthNamesShort: [
+            'Янв.',
+            'Февр.',
+            'Март',
+            'Апр.',
+            'Май',
+            'Июнь',
+            'Июль',
+            'Авг.',
+            'Сент.',
+            'Окт.',
+            'Нояб.',
+            'Дек.',
+        ],
+        dayNames: [
+            'Воскресенье',
+            'Понедельник',
+            'Вторник',
+            'Среда',
+            'Четверг',
+            'Птяница',
+            'Суббота',
+        ],
+        dayNamesShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        today: 'Сегодня',
+    };
+    LocaleConfig.defaultLocale = 'ru';
+
+    const initDate = new Date();
+    const [selected, setSelected] = useState(initDate);
     return (
+
         <Calendar
-            style={styles.container}
-            initialDate={new Date()}
-            minDate={'2022-05-30'}
-            maxDate={'2024-05-30'}
-            hideArrows={true}
-            theme={{
-                selectedDayBackgroundColor: '#1CBE8E1F',
-                selectedDayTextColor: COLORS.black,
-                textDayFontSize: 14,
-                textMonthFontSize: 14,
-                textDayHeaderFontSize: 14,
-                textDayFontFamily: 'interMedium',
-                textMonthFontFamily: 'interMedium',
-                textDayHeaderFontFamily: 'interMedium',
+            initialDate={ selected }
+            onDayPress={(day) => {
+                setSelected(day.dateString);
             }}
+            minDate="2022-05-30"
+            maxDate="2024-05-30"
+            firstDay={1}
+            hideArrows
+            theme={theme}
         />
     );
 }
