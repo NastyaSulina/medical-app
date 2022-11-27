bb4111fc3e46f242b0c55d2f3659697465e01a73
imgrau <sulina01@mail.ru>
2022-11-28 00:54:26 +0300

    calendar theme

--- src/components/CustomDatePicker/CustomDatePicker-styles.js
+++ src/components/CustomDatePicker/CustomDatePicker-styles.js
@@ -1,21 +1,59 @@
-import { StyleSheet } from 'react-native';
 import { COLORS } from '../../styles/globalStyles';
 
-const styles = StyleSheet.create({
-    container: {
-        borderRadius: 16,
-        minWidth: '90%',
-        minHeight: 340,
-        alignContent: 'center',
-        justifyContent: 'center',
-        shadowColor: '#212121',
-        shadowOffset: {
-            width: 0,
-            height: 2,
+const theme = {
+    selectedDayBackgroundColor: '#1CBE8E1F',
+    selectedDayTextColor: COLORS.black,
+    textDayFontSize: 14,
+    textMonthFontSize: 14,
+    weekVerticalMargin: 4,
+    todayTextColor: COLORS.green,
+    textDayHeaderFontSize: 14,
+    textDayFontFamily: 'interMedium',
+    textMonthFontFamily: 'interMedium',
+    textDayHeaderFontFamily: 'interMedium',
+    'stylesheet.calendar.header': {
+        week: {
+            borderTopColor: '#D6DEED',
+            borderTopWidth: 0.5,
+            paddingTop: 14,
+            flexDirection: 'row',
+            justifyContent: 'space-around',
         },
-        shadowOpacity: 0.04,
-        shadowRadius: 3,
     },
-});
+    'stylesheet.day.basic': {
+        base: {
+            width: 40,
+            height: 40,
+            alignItems: 'center',
+            alignSelf: 'center',
+            justifyContent: 'center',
+        },
+        selected: {
+            backgroundColor: '#1CBE8E1F',
+            borderRadius: 12,
+        },
+    },
+    'stylesheet.calendar.main': {
+        container: {
+            padding: 20,
+            backgroundColor: COLORS.white,
+            borderRadius: 16,
+            fontFamily: 'interMedium',
+            minWidth: '90%',
+            width: '100%',
+            shadowColor: '#212121',
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.04,
+            shadowRadius: 3,
+        },
+        dayContainer: {
+            flex: 1,
+            alignItems: 'center',
+        },
+    },
+}
 
-export default styles;
+export default theme;
