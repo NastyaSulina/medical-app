4f160fe4eeff5a773795999ec62a9d12474c31ff
imgrau <sulina01@mail.ru>
2022-12-18 19:06:32 +0300

    add popup for tasks

--- src/components/Task/Task-const.js
+++ src/components/Task/Task-const.js
@@ -1,4 +1,4 @@
-const TASK_TEXT = {
+export const TASK_TEXT = {
     symptom: {
         true: 'Отмечено',
         false: 'Отметить',
@@ -9,4 +9,40 @@ const TASK_TEXT = {
     },
 };
 
-export default TASK_TEXT;
+const getTemperature = () => {
+    const temperature = [];
+
+    for (let i = 32.0; i < 42.1; i += 0.1) {
+        temperature.push(i.toFixed(1));
+    }
+    return [temperature];
+}
+
+const getPressure = () => {
+    const pressure = [];
+
+    const upper = [];
+    const lower = [];
+    const heartbeat = [];
+
+    for (let i = 60; i < 300; i++) {
+        upper.push(i);
+    }
+
+    for (let i = 10; i < 150; i++) {
+        lower.push(i);
+    }
+
+    for (let i = 40; i < 220; i++) {
+        heartbeat.push(i);
+    }
+
+    pressure.push(upper, lower, heartbeat);
+
+    return pressure
+}
+
+export const WHEEL_OPTIONS = {
+    pressure: getPressure(),
+    temperature: getTemperature(),
+};
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,36 +1,99 @@
-import React from 'react';
-import { Image, View } from 'react-native';
-import { useDispatch } from 'react-redux';
-import { changeStatus } from '../../redux/actions';
+import React, {useState} from 'react';
+import {Image, View} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {changeStatus} from '../../redux/actions';
 import Button from '../Button/Button';
-import TASK_TEXT from './Task-const';
+import { TASK_TEXT, WHEEL_OPTIONS} from './Task-const';
 import ClockImage from '../../../assets/main-assets/clock.png';
 import styles from './Task-styles';
 import TextCustom from '../TextCustom/TextCustom';
-import { globalStyles } from '../../styles/globalStyles';
+import {globalStyles} from '../../styles/globalStyles';
+import Popup from '../Popup/Popup';
 
-function Task({ date, id, taskName, type, isChecked, time, outerStyles, units, numberPerUse }) {
+function Task({
+                  date,
+                  id,
+                  taskName,
+                  type,
+                  isChecked,
+                  time,
+                  outerStyles,
+                  units,
+                  numberPerUse,
+                  isDefault = true,
+              }) {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modalText, setModalText] = useState('Отметьте');
+    const [modalOptions, setModalOptions] = useState();
+    const [modalType, setModalType] = useState('radio');
+
     const dispatch = useDispatch();
 
     return (
-        <View style={[styles.container, globalStyles.shadow, outerStyles]}>
-            <View style={styles.taskInfo}>
-                <TextCustom text={taskName} outerStyles={styles.taskTitle} />
-                <View style={styles.timeInfo}>
-                    <Image style={styles.clockImage} source={ClockImage} resizeMode="contain" />
-                    <TextCustom outerStyles={styles.timeText} text={time} />
-                    <TextCustom outerStyles={styles.unitsText} text={`${numberPerUse} ${units}`} />
+        <>
+            <View style={[styles.container, globalStyles.shadow, outerStyles]}>
+                <View style={styles.taskInfo}>
+                    <TextCustom text={taskName} outerStyles={styles.taskTitle}/>
+                    <View style={styles.timeInfo}>
+                        <Image
+                            style={styles.clockImage}
+                            source={ClockImage}
+                            resizeMode="contain"
+                        />
+                        <TextCustom outerStyles={styles.timeText} text={time}/>
+                        {Boolean(numberPerUse) && (
+                            <TextCustom
+                                outerStyles={styles.unitsText}
+                                text={`${numberPerUse} ${units}`}
+                            />
+                        )}
+                    </View>
                 </View>
+                <Button
+                    text={TASK_TEXT[type][isChecked]}
+                    type={isChecked ? 'checked' : 'primary'}
+                    size="M"
+                    textFont="semiBold"
+                    opacity={0.6}
+                    onPress={() => {
+                        if (type === 'medicine') {
+                            dispatch(changeStatus({id, date}));
+                        } else if (
+                            type === 'symptom' &&
+                            isDefault &&
+                            taskName === 'Настроение'
+                        ) {
+                            setModalText('Как ваше настроение?');
+                            setModalType('slider');
+                            setModalVisible(!modalVisible);
+                        } else if (
+                            type === 'symptom' &&
+                            isDefault &&
+                            taskName === 'Давление'
+                        ) {
+                            setModalText('Введите ваше давление');
+                            setModalType('wheel');
+                            setModalOptions(WHEEL_OPTIONS.pressure)
+                            setModalVisible(!modalVisible);
+                        } else if (
+                            type === 'symptom' &&
+                            isDefault &&
+                            taskName === 'Температура'
+                        ) {
+                            setModalText('Введите вашу температуру');
+                            setModalType('wheel');
+                            setModalOptions(WHEEL_OPTIONS.temperature)
+                            setModalVisible(!modalVisible);
+                        } else if (type === 'symptom' && !isDefault) {
+                            setModalText(`Отметьте ${taskName}!`);
+                            setModalType('radio');
+                            setModalVisible(!modalVisible);
+                        }
+                    }}
+                />
             </View>
-            <Button
-                text={TASK_TEXT[type][isChecked]}
-                type={isChecked ? 'checked' : 'primary'}
-                size="M"
-                textFont="semiBold"
-                opacity={0.6}
-                onPress={() => dispatch(changeStatus({ id, date }))}
-            />
-        </View>
+            {modalVisible && <Popup text={modalText} type={modalType} wheelOptions={modalOptions}/>}
+        </>
     );
 }
 
