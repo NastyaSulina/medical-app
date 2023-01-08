1d99f531bc25b5eb491afd7748d5616c7ac47b21
imgrau <sulina01@mail.ru>
2023-01-08 21:04:55 +0300

    fix background updating and separator behaviour

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -4,27 +4,29 @@ import styles from './TaskList-styles';
 import Task from '../Task/Task';
 
 function TaskList({ tasks, status, date }) {
+
+    const otherStatusTasks = tasks.filter((task) => task.status === !status);
+    const thisStatusTasks = tasks.filter((task) => task.status === status);
+
     return (
         <>
-            {status && <View style={styles.separator} />}
-            {Boolean(tasks) &&
-                tasks
-                    .filter((task) => task.status === status)
-                    .map((task) => (
-                        <Task
-                            key={task.id}
-                            type={task.type}
-                            id={task.id}
-                            date={date}
-                            units={task.units}
-                            numberPerUse={task.numberPerUse}
-                            taskName={task.name}
-                            isChecked={task.status}
-                            time={task.time}
-                            outerStyles={styles.task}
-                            isDefault={task.default}
-                        />
-                    ))}
+            {status && thisStatusTasks.length > 0 && otherStatusTasks.length > 0 && <View style={styles.separator} />}
+            {thisStatusTasks.length > 0 && thisStatusTasks
+                .map((task) => (
+                    <Task
+                        key={task.id}
+                        type={task.type}
+                        id={task.id}
+                        date={date}
+                        units={task.units}
+                        numberPerUse={task.numberPerUse}
+                        taskName={task.name}
+                        isChecked={task.status}
+                        time={task.time}
+                        outerStyles={styles.task}
+                        isDefault={task.default}
+                    />
+                ))}
         </>
     );
 }
